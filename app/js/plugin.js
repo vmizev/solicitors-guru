8a222a09616df148c74ce7521f9d0b026d4124b1
vmizev <valentin.mizev@gmail.com>
2017-02-08 19:03:12 +0200

    добавлен адаптив

--- app/js/plugin.js
+++ app/js/plugin.js
@@ -8,13 +8,49 @@
 	})
 })(jQuery);
 
+//search form header
+(function($) {
+	var searchIco = $(".search-ico");
+	var searchBlock = $(".search-block");
+	
+	searchIco.on("click", function() {
+		searchBlock.toggle("slide");
+		searchIco.toggleClass("close");
+	});
+
+}) (jQuery);
+
+// init SlickNav
+$('.header-nav').slicknav({
+	label: '',
+	prependTo:'.header .container',
+});
+
 // slick carousel init
 $('.areas-list').slick({
 	dots: false,
-	slidesToShow: 7,
+	slidesToShow: 5,
 	slidesToScroll: 1,
 	autoplay: true,
 	autoplaySpeed: 2000,
+	centerPadding: '0px',
+	centerMode: true,
+	responsive: [
+		{
+			breakpoint: 959,
+			settings: {
+				slidesToShow: 3,
+			}
+			
+		},
+		{
+			breakpoint: 479,
+			settings: {
+				slidesToShow: 1,
+			}
+			
+		},		
+	]
 });
 
 // init Formstyler
